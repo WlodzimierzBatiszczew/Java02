433de2f8e468d9ffd9b574c0e6385c77190d2582
WlodzimierzBatiszczew <123464547+WlodzimierzBatiszczew@users.noreply.github.com>
2024-02-01 16:13:38 +0200

    hw0102

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,10 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        int a = 10;
+        int b = 23;
+        int c = a + b;
+        System.out.println(c);
     }
 }
